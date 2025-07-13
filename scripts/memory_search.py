dea12b6fa7282bb19998754442a611cc3d8ea664
Shadoukun <shadoukun@gmail.com>
2025-07-12 21:57:05 -0400

    improve search script and import config from bot instead of keeping its own

--- scripts/memory_search.py
+++ scripts/memory_search.py
@@ -1,34 +1,11 @@
+import os
+import sys
 import argparse
 from typing import Any
 from mem0 import Memory
 
-MODEL_NAME = "huihui_ai/qwen3-abliterated:8b"  # or any other model you want to use
-
-memory_config = {
-    "vector_store": {
-        "provider": "chroma",
-        "config": {
-            "collection_name": "memory",
-            "path": "db",
-        }
-    },
-    "llm": {
-        "provider": "ollama",
-        "config": {
-            "model": MODEL_NAME,
-            "temperature": 0.6,
-            "max_tokens": 2000,
-            "ollama_base_url": "http://localhost:11434",
-        },
-    },
-    "embedder": {
-        "provider": "ollama",
-        "config": {
-            "model": "nomic-embed-text:latest",
-            "ollama_base_url": "http://localhost:11434",
-        },
-    },
-}
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from AIBot.agents import memory_config  
 
 memory = Memory.from_config(memory_config)
 
@@ -43,10 +20,9 @@ def delete_memories(memories: list[dict[str, Any]]) -> str:
         memory_id, memory_content = memory_entry.get("id"), memory_entry.get("memory")
         if memory_id and memory_content:
             res = memory.delete(memory_id)  # type: ignore
-            if res.get("success"):
-                print(f"Deleted memory with ID: {memory_id} - {memory_content}")
-            else:
-                print(f"Failed to delete memory with ID: {memory_id}")
+            for id, m in res.items():
+                print(f"Deleted memory {id}: {m}")
+
     return "Deletion process completed."
 
 def main():
@@ -60,7 +36,7 @@ def main():
     if args.query:
         res = search_memory(args.query, agent_id=args.agent_id, limit=args.limit)
         for entry in res:
-            print(f"ID: {entry['id']}, Memory: {entry['memory']}")
+            print(f"ID: {entry['id']}, | {entry['memory']}")
         if args.delete:
             msg = delete_memories(res)
             print(msg)
